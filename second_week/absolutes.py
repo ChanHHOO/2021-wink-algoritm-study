abc602b72c646802c0426843f79b4d80cc60cf82
ChanHHOO <hpyho33@kookmin.ac.kr>
2021-07-29 14:45:58 +0000

    absolutes upload

---
+++ second_week/absolutes.py
@@ -0,0 +1,8 @@
+def solution(absolutes, signs):
+    answer = 123456789
+    for i in range(len(absolutes)):
+        if signs[i] == False:
+            absolutes[i] *= -1
+    answer = sum(absolutes)
+    
+    return answer
