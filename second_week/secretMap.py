fb8219f44e40561bd31396d7d80ca8bf31cb975c
ChanHHOO <hpyho33@kookmin.ac.kr>
2021-07-29 14:31:06 +0000

    secretMap upload

--- second_week/secretMap.py
+++ second_week/secretMap.py
@@ -29,7 +29,6 @@ def solution(n, arr1, arr2):
             else:
                 final_list[i] += " "
     
-    print(final_list)
     return final_list
 
 solution(6, [46, 33, 33 ,22, 31, 50], [27 ,56, 19, 14, 14, 10])
